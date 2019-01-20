88b6c2f47dc103bc9f0c93abe374c36e1d2596c7
DESKTOP-GI6DUSV\User <yevgeny@anzu.io>
2019-01-20 15:59:58 +0200

    First commit

---
+++ Assets/Scripts/OverlayController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayController : MonoBehaviour
+{
+    Animator animator;
+    bool isAlreadyDeactivated = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnLinkClick()
+    {
+        Application.OpenURL("https://wiki.anzu.io/unity-integration-guide");
+    }
+
+    public void OnOverlayClick()
+    {
+        if (isAlreadyDeactivated)
+            return;
+        
+        animator.SetTrigger("Deactivate");
+        isAlreadyDeactivated = true;
+    }
+
+    void DeactivateObject()
+    {
+        gameObject.SetActive(false);
+    }
+}
