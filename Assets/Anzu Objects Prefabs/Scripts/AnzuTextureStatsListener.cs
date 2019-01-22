9547318964e61b0f9e04dd2fb63d92382b94acc0
DESKTOP-GI6DUSV\User <yevgeny@anzu.io>
2019-01-22 18:13:45 +0200

    improved the example project added the possibility to mouse click on a placement to see its texture stats, or use space to toggle stats on all placements alltogether

---
+++ Assets/Anzu Objects Prefabs/Scripts/AnzuTextureStatsListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using anzu;
+
+
+[RequireComponent(typeof(AnimatedTextureStats))]
+public class AnzuTextureStatsListener : MonoBehaviour
+{
+    AnimatedTextureStats PlacementStats;
+
+    private void Awake()
+    {
+        PlacementStats = GetComponent<AnimatedTextureStats>();
+    }
+
+    private void OnMouseDown()
+    {
+        PlacementStats.enabled = !PlacementStats.enabled;
+    }
+}
---
+++ Assets/Anzu Objects Prefabs/Scripts/AnzuTextureStatsManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using anzu;
+
+
+public class AnzuTextureStatsManager : MonoBehaviour
+{
+    [HideInInspector] public AnimatedTextureStats[] PlacementsStats;
+
+    bool showTextureStats = false;
+    public bool ShowTextureStats
+    {
+        get
+        {
+            return showTextureStats;
+        }
+        set
+        {
+            showTextureStats = value;
+
+            foreach (AnimatedTextureStats stats in PlacementsStats)
+            {
+                stats.enabled = showTextureStats;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        PlacementsStats = FindObjectsOfType<AnimatedTextureStats>();
+
+        foreach (AnimatedTextureStats stats in PlacementsStats)
+        {
+            if (stats.GetComponent<AnzuTextureStatsListener>() == null)
+            {
+                stats.gameObject.AddComponent<AnzuTextureStatsListener>();
+            }
+        }
+    }
+
+    private void Start()
+    {
+        ShowTextureStats = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowTextureStats = !ShowTextureStats;
+        }
+    }
+}
