442edac038a9823b3eba325aa83cb37859125cfb
DESKTOP-GI6DUSV\User <yevgeny@anzu.io>
2020-02-26 15:16:43 +0200

    minor changes

---
+++ Assets/Scripts/ProfilingScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using UnityEngine.UI;
+using UnityEngine.Profiling;
+
+
+
+public class ProfilingScript : MonoBehaviour
+{
+    //PerformanceCounter cpuCounter;
+    //PerformanceCounter ramCounter;
+
+    //void Start()
+    //{
+    //    cpuCounter = new PerformanceCounter();
+
+    //    cpuCounter.CategoryName = "Processor";
+    //    cpuCounter.CounterName = "% Processor Time";
+    //    cpuCounter.InstanceName = "_Total";
+
+    //    ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+    //}
+
+    //void Update()
+    //{
+    //    print(getCurrentCpuUsage());
+    //    print(getAvailableRAM());
+    //}
+
+    //public string getCurrentCpuUsage()
+    //{
+    //    return cpuCounter.NextValue() + "%";
+    //}
+
+    //public string getAvailableRAM()
+    //{
+    //    return ramCounter.NextValue() + "MB";
+    //}
+}
